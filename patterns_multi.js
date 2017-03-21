038701e05be995b681e8ca859056860c976a94e0
Emily Woods <emilymonica.woods@gmail.com>
2017-03-21 22:21:03 +0000

    Making geometric patterns with p5js

---
+++ patterns_multi.js
@@ -0,0 +1,44 @@
+var radius = 100;
+var radian = Math.PI / 180;
+var frames = 0;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  colorMode(HSB);
+  background(0);
+}
+
+function draw() {
+  background(0);
+  frames +=0.00005;
+  var k = Math.sin(frames*radius);
+  var rad_update = radius * Math.abs(k);
+  var l = 100-((100 * Math.abs(k))/2);
+  stroke (0,10,l);
+
+  for (var i = 0; i < width+100; i += 50) {
+    for (var j = 0; j < height+100; j += 50) {
+      draw_circle(i, j, rad_update)
+    }
+  }
+  for (var i = 0; i < width; i += 100) {
+    for (var j = 0; j < height; j += 100) {
+      draw_triangle(0 + i , 0  +j)
+    }
+  }
+}
+
+function draw_circle(x, y, radius) {
+  noFill()
+  stroke((radius+random(660)/2)-100,68,66)
+  strokeWeight(2);
+  ellipse(x, y, radius-2);
+  strokeWeight(1);
+  stroke('white')
+  ellipse(x, y, radius);
+}
+
+function draw_triangle(x, y) {
+  stroke('white')
+  triangle(x, y, x + 50, y - 100, x + 100, y)
+}
